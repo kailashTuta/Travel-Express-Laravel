df596b89bd365b3e8683f2ebdc4d247aca1bf367
kailashTuta <kailashTuta@users.noreply.github.com>
2021-03-03 20:37:11 +0530

    Reorganized Folders

--- resources/views/templates/gallery.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.master')
-@section('title')
-    Travel Express | Gallery
-@endsection
-@section('content')
-    @include('layouts.navbar')
-    @include('layouts.gallerySection')
-    @include('layouts.footer')
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('templates.home');
+    return view('layouts.index');
 });
 Route::get('/gallery', function () {
-    return view('templates.gallery');
+    return view('layouts.gallery');
 });
